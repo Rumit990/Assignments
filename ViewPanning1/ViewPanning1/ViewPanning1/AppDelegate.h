35e1c8cede096577c58d5f2d8457f6ba11f385e7
Rumit Singh <rumittuteja@gmail.com>
2012-11-06 14:49:37 +0530

    Assignment : Panning View Gesture (1)

---
+++ ViewPanning1/ViewPanning1/ViewPanning1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  VIewPanning
+//
+//  Created by Chetan Sanghi on 06/11/12.
+//  Copyright (c) 2012 sanghichetan@yahoo.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
